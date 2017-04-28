a37240e56577102c1f0e5da2d459252753cf25bc
EGOIST <0x142857@gmail.com>
2017-04-28 22:39:43 +0800

    Site updated: 2017-04-28 22:39:42

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ var $$ = document.querySelectorAll.bind(document)
 document.addEventListener('DOMContentLoaded', ready)
 
 function ready() {
-  dropdown()
   // disqus
   if (window.isPost) {
     disqus()
@@ -22,23 +21,6 @@ function ready() {
   function makeZoom() {
     Array.prototype.forEach.call($$('.post-content img'), function (el) {
       zoom(el)
-    })
-  }
-  function dropdown() {
-    var el = $('.nav-more-trigger')
-    var list = $('.dropdown-list')
-    var menu = $('.dropdown-menu')
-    el.addEventListener('click', function (e) {
-      if (list.classList.contains('visible')) {
-        list.classList.remove('visible')
-      } else {
-        list.classList.add('visible')
-      }
-    })
-    document.addEventListener('click', function (e) {
-      if (!menu.contains(e.target)) {
-        list.classList.remove('visible')
-      }
     })
   }
 }
